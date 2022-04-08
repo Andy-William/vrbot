d48b83df2673c221a8e00e5a9de12135c8c2bcd4
Andy William <Andy-William@users.noreply.github.com>
2022-04-08 08:33:16 +0000

    dreay cat rename

--- cron/maintenance_done.js
+++ cron/maintenance_done.js
@@ -2,7 +2,7 @@ const announce = require('./../lib/announce.js');
 
 module.exports = {
 	name: 'maintenance done reminder',
-	schedule: '0 21 28 3 *',
+	schedule: '0 17 28 3 *',
 	async action() {
     announce("🚧 Maintenance should be done! 🚧")
 	},
