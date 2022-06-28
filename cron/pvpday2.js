db2588e1c16dae1336d13093d7a919e397cb3f3e
Andy William <Andy-William@users.noreply.github.com>
2022-06-28 11:43:32 +0000

    pvp dates

--- cron/pvpday2.js
+++ cron/pvpday2.js
@@ -3,12 +3,12 @@ const announce = require('./../lib/announce.js');
 
 module.exports = {
 	name: '12v12 rank day reminder',
-	schedule: '0 12 1-21 6 Sat',
+	schedule: '0 12 2-16 7 Sat',
 	async action() {
     // const weekNum = currentWeek.week(new Date());
     // if( weekNum >= 2674 && weekNum <=2675 ) announce("<:pvpr:678097570618408961> Hari ini ada **PvP Rank** <:pvpr:678097570618408961>")
     // const weekCycle= (currentWeek.week(new Date())-2)%10;
-    // announce("<:pvpr:678097570618408961> Today's event is **Holy Ground War** <:pvpr:678097570618408961>")
-    announce("<:pvpr:678097570618408961> Today's event is **PvP Rank** <:pvpr:678097570618408961>")
+    announce("<:pvpr:678097570618408961> Today's event is **Holy Ground War** <:pvpr:678097570618408961>")
+    // announce("<:pvpr:678097570618408961> Today's event is **PvP Rank** <:pvpr:678097570618408961>")
 	},
 };
