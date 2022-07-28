2150bf8970177f9adccd22c57c7b414fe4a1cbb4
Andy William <Andy-William@users.noreply.github.com>
2022-07-28 16:52:16 +0000

    remove normalm esage log

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const client = require('./lib/bot.js').client;
 
 const prefix = '!';
 client.on('messageCreate', (message) => {
-  let log = `${message.channel.id}-(${message.author.id}) ${message.author.username}: ${message.content}`
-  if( message.guild ) log = `[${message.guild.id}-${message.guild.name}]` + log
+  // let log = `${message.channel.id}-(${message.author.id}) ${message.author.username}: ${message.content}`
+  // if( message.guild ) log = `[${message.guild.id}-${message.guild.name}]` + log
   const errorHandler = function(err){
     let msg;
     try{
