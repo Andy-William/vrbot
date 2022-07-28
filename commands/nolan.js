d5c8acdd89bae23834bab09adb59ddeda04f17b8
Andy <frozen_altar@yahoo.co.id>
2022-07-28 12:29:07 +0700

    nolan

--- commands/nolan.js
+++ commands/nolan.js
@@ -10,20 +10,30 @@ async function odds(){
   return this.odds
 }
 
+async function getNolan(){
+  const chance = await odds();
+  let random = Math.random();
+  let rarity;
+  if( random < chance[0] ) rarity = 1
+  else rarity = 4
+
+  const card = (await db.getRandom('cards', {nolan: rarity}))[0];
+  return card;
+}
+
 module.exports = {
 	name: 'nolan',
 	description: 'Combined Fate Simulator',
-	async execute(message, args) {
-    const chance = await odds();
-    let random = Math.random();
-    let rarity;
-    if( random < chance[0] ) rarity = 1
-    else rarity = 4
-    
-    const card = (await db.getRandom('cards', {nolan: rarity}))[0];
+	async processMessage(message, args) {
+    const card = await getNolan();
     let str = "<:nolan:765856406540124171> Combined Fate Simulator <:nolan:765856406540124171>\n";
     str += `${message.author.toString()} got **${card.name}** - priced ${(card.price||0).toLocaleString()}\n`;
-
-    return message.channel.send(str);
+    await message.channel.send(str);
 	},
+  async processInteraction(interaction){
+    const card = await getNolan();
+    let str = "<:nolan:765856406540124171> Combined Fate Simulator <:nolan:765856406540124171>\n";
+    str += `${interaction.user.toString()} got **${card.name}** - priced ${(card.price||0).toLocaleString()}\n`;
+    await interaction.reply(str);
+  }
 };
