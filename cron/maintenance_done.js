f426898de9606afdeca676fb8bcd8070db036be7
andy.william <andy.william@wartek.belajar.id>
2023-10-07 12:22:13 +0700

    remove  crons

--- cron/maintenance_done.js
+++
@@ -1,9 +0,0 @@
-const announce = require('../lib/announce.js');
-
-module.exports = {
-	name: 'maintenance done reminder',
-	schedule: '0 19 19 4 *',
-	async action() {
-    announce("🚧 Maintenance should be done! 🚧")
-	},
-};
