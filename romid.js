7fe2bb23c3facadb2dc80c4e987cacaf945f6f97
andy.william <andy.william@wartek.belajar.id>
2024-03-09 15:40:02 +0700

    cache

--- romid.js
+++ romid.js
@@ -1,17 +1,29 @@
 const fetch = require('node-fetch');
+const cache = require('./lib/cache.js');
 
 module.exports = function(app){
   app.get("/rom/:id", async (request, response) => {
     const ip = (request.headers['x-forwarded-for'] || request.connection.remoteAddress || '').split(',')[0].trim();
     console.log('rom from', ip, request.params.id);
-    await fetch("https://romhandbook.com/things/" + request.params.id).then(res => res.text()).then(data=>{
-      const nameRegex = /font-semibold leading-6.*\n(.*)/;
-      match = nameRegex.exec(data)
-      console.log(match[1])
+    cacheKey = 'rom-' + request.params.id
+    ans = cache.get(cacheKey)
+    if( !ans ){
+      await fetch("https://romhandbook.com/things/" + request.params.id).then(res => res.text()).then(data=>{
+        const nameRegex = /font-semibold leading-6.*\n(.*)/;
+        match = nameRegex.exec(data)
+        console.log(match[1])
+        cache.set(cacheKey, match[1])
+        response.set({
+          'Access-Control-Allow-Origin': '*'
+        })
+        response.send(match[1])
+      })
+    } else {
+      console.log('cached ', ans)
       response.set({
         'Access-Control-Allow-Origin': '*'
       })
-      response.send(match[1])
-    })
+      response.send(ans)
+    }
   });
 }
